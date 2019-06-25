082ece965711d6d7a6579a77d77c18ac860f677f
Leffie <leonwow@live.com.pt>
2019-06-25 17:58:44 +0100

    Audio files Case Study

--- R/imagetest.R
+++ R/imagetest.R
@@ -39,19 +39,20 @@ X <- cbind(X,Z)
 initw <- matrix(c(0.86456941, 1.0646533 , 0.07053282 ,0.8470662), 2, 2 , TRUE)
 #initw<- matrix(rnorm(2^2),2,2)
 #PCA Whitening to match Julia implementation
-S = cov(X)
-W = whiteningMatrix (S, method="PCA")
-X1 = tcrossprod(X, W) # whitened data
+#= S = cov(X)
+#W = whiteningMatrix (S, method="PCA")
+#X1 = tcrossprod(X, W) # whitened data
 
-a <- ica.R.def(t(X1), 2, fun = "logcosh", alpha = 1, maxit = 200,
-               tol = 0.0001, verbose = TRUE, initw)
-K = a%*%W
-S = X%*%t(K)
-S = t(S)
+#a <- ica.R.def(t(X1), 2, fun = "logcosh", alpha = 1, maxit = 200,
+               #tol = 0.0001, verbose = TRUE, initw)
+#K = a%*%W
+#S = X%*%t(K)
+#S = t(S)
 
 
-#a <- fastICA(K, 2, alg.typ = "deflation", fun = "logcosh", alpha = 1,
-             #method = "R", row.norm = FALSE, maxit = 100,
-             #tol = 0.0001, verbose = TRUE)
+r <- fastICA(X, 2, alg.typ = "deflation", fun = "logcosh", alpha = 1,
+             method = "R", row.norm = FALSE, maxit = 200 ,
+             tol = 0.0001 , verbose = TRUE)
+
 plot(as.cimg(matrix(S[1,]+S[2,],rows,cols)), main = "Cat one")
 plot(as.cimg(matrix(S[1,]-S[2,],rows,cols)), main = "Cat two")
